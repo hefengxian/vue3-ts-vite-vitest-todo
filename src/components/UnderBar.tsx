31ede00641f527d1cfba052cc24f02de10d126ec
Frank <jssmith883@gmail.com>
2022-02-22 01:39:26 +0300

    Create UnderBar component to action todo list

---
+++ src/components/UnderBar.tsx
@@ -0,0 +1,39 @@
+import { defineComponent, PropType } from "vue"
+import { store } from "../Storage"
+
+export default defineComponent({
+  name: 'UnderBar',
+  props: {
+    filterType: {
+      type: String as PropType<filterType>,
+      default: 'all',
+    },
+    filteredCount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  emits: [
+    'update:filterType',
+  ],
+  setup(props, {emit}) {
+    let todos = store.state.todos
+
+    const filterChange = (type: filterType) => emit('update:filterType', type)
+
+    return () => (
+      <div class="footer">
+        <div class="info">{props.filteredCount}/{todos.length}</div>
+        <div class="filters">
+          <button class={props.filterType === 'all' ? 'selected' : ''} onClick={() => filterChange('all')}>All</button>
+          <button class={props.filterType === 'active' ? 'selected' : ''} onClick={() => filterChange('active')}>Active</button>
+          <button class={props.filterType === 'done' ? 'selected' : ''} onClick={() => filterChange('done')}>Done</button>
+        </div>
+        <div class="clear-all">
+          <button onClick={() => store.state.todos = todos.filter(t => !t.done)}>Clear Done</button>
+        </div>
+      </div>
+    )
+  }
+})
