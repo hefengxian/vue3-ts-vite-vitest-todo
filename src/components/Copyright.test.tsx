526c980281651fefdec990aae33e491a877e1bea
Frank <jssmith883@gmail.com>
2022-02-22 01:35:50 +0300

    Add Copyright component & test case

---
+++ src/components/Copyright.test.tsx
@@ -0,0 +1,12 @@
+import { mount } from "@vue/test-utils"
+import { expect, test } from "vitest"
+import Copyright from "./Copyright"
+
+const wrapper = mount(Copyright)
+
+test('Should render', async () => {
+  expect(wrapper.find('.copyright').findAll('p').map(p => p.text())).toEqual([
+    'Created by Frank',
+    'Powered by Vue 3 & TypeScript',
+  ])
+})
