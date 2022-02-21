4d9a95093cdb4d0c9d84a6fca107d05f0df14759
Frank <jssmith883@gmail.com>
2022-02-22 01:31:22 +0300

    Add Utils.js to put some common functions in there

---
+++ src/Utils.test.ts
@@ -0,0 +1,25 @@
+import { test, describe, expect } from 'vitest'
+import { uuid } from './Utils'
+
+describe('UUID', () => {
+  test('By default should generate random 8 length by base62', () => {
+    const results: string[] = []
+    for (let i = 0; i < 1000; i++) {
+      results.push(uuid())
+    }
+    results.forEach(v => {
+      expect(v).toMatch(/^[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789]{8}$/)
+    })
+  })
+
+  test('By default should generate random 16 length by base62', () => {
+    const results: string[] = []
+    for (let i = 0; i < 1000; i++) {
+      results.push(uuid(16))
+    }
+    results.forEach(v => {
+      expect(v).toMatch(/^[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789]{16}$/)
+    })
+  })
+})
+
---
+++ src/Utils.ts
@@ -0,0 +1,9 @@
+export function uuid(len: number = 8): string {
+  let uuid = ''
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (let i = 0; i < len; i++) {
+    uuid += chars.charAt(Math.floor(Math.random() * chars.length))
+  }
+  return uuid
+}
+
